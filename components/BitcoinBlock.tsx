7c51f9ab9c0673e7c3f46849ced30680a711c543
Agustin Kassis <webmaster@masize.com>
2022-11-04 10:31:12 -0300

    block count draft

--- components/BitcoinBlock.tsx
+++ components/BitcoinBlock.tsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { BitcoinContext } from "../contexts/Bitcoin";
 
 const Container = styled.div``;
 
+const BLOCK_HIT = process.env.NEXT_PUBLIC_BLOCK_START;
+
 const BitcoinBlock = () => {
+  const { blockNumber } = useContext(BitcoinContext);
   return (
     <Container>
-      <p>Esperando bloque de Bitcoin #43.432...</p>
+      <div>Bloque actual: {blockNumber}</div>
+      <p>Esperando bloque de Bitcoin #{BLOCK_HIT}...</p>
     </Container>
   );
 };
