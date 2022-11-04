6f416ecb66fbfb0d98a24c842beb0d289540618a
Agustin Kassis <webmaster@masize.com>
2022-11-04 10:41:58 -0300

    getWinners mock

--- contexts/Players.tsx
+++ contexts/Players.tsx
@@ -4,10 +4,12 @@ import { Player } from "../types/players";
 interface IPlayersContext {
   total?: number;
   players: Player[];
+  getWinners: () => void;
 }
 
 export const PlayersContext = createContext<IPlayersContext>({
   players: [],
+  getWinners: () => {},
 });
 
 interface IPlayersProviderProps {
@@ -31,12 +33,17 @@ export const PlayersProvider = ({ children }: IPlayersProviderProps) => {
   const [total, setTotal] = useState<number>(60);
   const [players, setPlayers] = useState<Player[]>([]);
 
+  const getWinners = () => {
+    console.info("Needs to implement Get Winners!");
+  };
+
   useEffect(() => {
     setPlayers(generateBlankPlayers(total));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
-    <PlayersContext.Provider value={{ total, players }}>
+    <PlayersContext.Provider value={{ total, players, getWinners }}>
       {children}
     </PlayersContext.Provider>
   );
