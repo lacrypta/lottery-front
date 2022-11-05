09be530bf6fcc6c9fd675e1a60fcaef802f7e852
Agustin Kassis <webmaster@masize.com>
2022-11-05 20:39:36 -0300

    show only winners on finished

--- components/Steps/Finished/Finished.tsx
+++ components/Steps/Finished/Finished.tsx
@@ -1,15 +1,18 @@
 import WaitingBlock from "../../WaitingBlock";
 import CardGrid from "../../CardGrid";
 import Logo from "../../Logo";
+import { useContext } from "react";
+import { PlayersContext } from "../../../contexts/Players";
 
 const Finished = () => {
+  const { winners } = useContext(PlayersContext);
   return (
     <>
       <Logo />
       <h1>Finished!</h1>
       <WaitingBlock />
 
-      <CardGrid />
+      <CardGrid onlyWinners={true} winners={winners || []} />
     </>
   );
 };
