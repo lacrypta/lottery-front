370403b6db3fe12aadbe4314dd67a54c8f6c629b
Agustin Kassis <webmaster@masize.com>
2022-11-05 19:21:02 -0300

    start process with delay

--- components/Steps/Play/Play.tsx
+++ components/Steps/Play/Play.tsx
@@ -13,26 +13,37 @@ const delay = (time: number) => {
 
 const Play = () => {
   const { blockHash } = useContext(BitcoinContext);
-  const { winners } = useContext(PlayersContext);
-  const { lotteryDelay } = useContext(ConfigContext);
+  const { total, winners } = useContext(PlayersContext);
+  const { lotteryDelay, staggeringDelay } = useContext(ConfigContext);
 
   const [started, setStarted] = useState(false);
+
   const [winnersShown, setWinnersShown] = useState<number[]>([]);
   const [showWinnerInterval, setShowWinnerInterval] = useState<NodeJS.Timer>();
   const [winnerIndex, setWinnerIndex] = useState<number>(0);
 
   let mounted = false;
 
-  async function startProcess() {
-    if (!winners) {
-      return;
-    }
-
+  async function startLottery() {
     setShowWinnerInterval(
       setInterval(() => {
         setWinnerIndex((i) => ++i);
       }, lotteryDelay)
     );
+  }
+
+  async function onFinishedLottery() {
+    console.info("Finished lottery");
+  }
+
+  async function startProcess() {
+    if (!winners || !staggeringDelay || !lotteryDelay) {
+      return;
+    }
+
+    setTimeout(() => {
+      startLottery();
+    }, staggeringDelay * total + lotteryDelay);
   }
 
   // New Winner interval
@@ -42,6 +53,7 @@ const Play = () => {
     }
     if (winnerIndex >= winners.length) {
       clearInterval(showWinnerInterval);
+      onFinishedLottery();
       return;
     }
     setWinnersShown((old) => [...old, winners[winnerIndex]]);
