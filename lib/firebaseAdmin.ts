f9494010ee8d2c4a7e2f35aec9ac9be2616c2649
Agustin Kassis <webmaster@masize.com>
2022-11-08 11:30:59 -0300

    added firebase admin lib

---
+++ lib/firebaseAdmin.ts
@@ -0,0 +1,63 @@
+import { initializeApp, cert } from "firebase-admin/app";
+
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT ?? "{}");
+
+// Initialize Firebase
+try {
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
+} catch (err: any) {
+  // we skip the "already exists" message which is
+  // not an actual error when we're hot-reloading
+  if (!/already exists/.test(err.message)) {
+    console.error("Firebase initialization error", err);
+  }
+}
+
+const db = getFirestore();
+
+// **** FUNCTIONS **** //
+
+/**
+ * Adds new word to dictionary
+ * @param {String} word
+ * @returns
+ */
+export const log = async (type: string, data: any) => {
+  return await db.collection("log").add({
+    time: FieldValue.serverTimestamp(),
+    type: type,
+    data: data,
+  });
+};
+
+/**
+ * Get config
+ * @returns
+ */
+export const getConfig = async () => {
+  const configRef = db.collection("config").doc("main");
+  const doc = await configRef.get();
+  if (!doc.exists) {
+    log("Config", "Does not exist");
+    return false;
+  }
+  return doc.data();
+};
+
+/**
+ * Set block number
+ * @param {number} blockNumber
+ * @returns
+ */
+export const setBlockTarget = async (blockNumber: number) => {
+  return db
+    .collection("config")
+    .doc("main")
+    .update({ blockTarget: blockNumber });
+};
+
+exports.log = log;
