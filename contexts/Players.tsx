3ec337bed08921ef5a79cf48c26bb0effa44a74a
Agustin Kassis <webmaster@masize.com>
2022-11-11 03:07:01 -0300

    updated Players context

--- contexts/Players.tsx
+++ contexts/Players.tsx
@@ -7,12 +7,15 @@ import { Lottery, ILottery } from "@lacrypta/lottery/typechain-types";
 import { BitcoinContext } from "./Bitcoin";
 import { ajaxCall } from "../lib/request";
 import { CreateLotteryRequest } from "../types/request";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../lib/firebase";
 
 const { abi: lotteryAbi, address: contractAddress } = deployedLottery;
 
 interface IPlayersContext {
   total: number;
-  winners?: number[];
+  winners?: string[];
+  players?: string[];
   getWinners: () => void;
 }
 
@@ -33,11 +36,20 @@ function generateArray(total: number) {
   return res;
 }
 
+async function generatePlayers(): Promise<string[]> {
+  const playersRef = collection(db, "players");
+  const q = query(playersRef);
+
+  const docs = (await getDocs(q)).docs;
+  return docs.map((doc) => doc.data().name);
+}
+
 export const PlayersProvider = ({ children }: IPlayersProviderProps) => {
   const { totalPlayers, totalWinners, lotteryName, txHash } =
     useContext(ConfigContext);
-  const [winners, setWinners] = useState<number[]>([]);
+  const [winners, setWinners] = useState<string[]>([]);
   const { blockHash } = useContext(BitcoinContext);
+  const [players, setPlayers] = useState<string[]>([]);
 
   const provider = useProvider();
 
@@ -51,12 +63,15 @@ export const PlayersProvider = ({ children }: IPlayersProviderProps) => {
     const lotteryConfig: ILottery.ConfigStruct = {
       seed: "0x" + blockHash,
       numberOfWinners: totalWinners as number,
-      players: generateArray(totalPlayers as number),
+      players,
     };
 
     const _winners = await contract["simulate((bytes32,uint256,string[]))"](
       lotteryConfig
     );
+
+    console.info("Winners:");
+    console.dir(_winners);
 
     const requestData: CreateLotteryRequest = {
       name: lotteryName as string,
@@ -67,12 +82,24 @@ export const PlayersProvider = ({ children }: IPlayersProviderProps) => {
       ajaxCall("publish", requestData);
     }
 
-    setWinners(_winners.map((n: string) => parseInt(n)));
+    setWinners(_winners);
   };
+
+  useEffect(() => {
+    // setPlayers(generateArray(totalPlayers as number));
+    generatePlayers().then((_players) => {
+      setPlayers(_players);
+    });
+  }, []);
 
   return (
     <PlayersContext.Provider
-      value={{ total: totalPlayers || 1, winners, getWinners }}
+      value={{
+        total: players.length > 0 ? players.length : totalPlayers || 1,
+        winners,
+        players,
+        getWinners,
+      }}
     >
       {children}
     </PlayersContext.Provider>
