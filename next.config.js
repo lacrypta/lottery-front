82a2bbaedb8870936ead6ba7cb33a5915d2f96e4
Agustin Kassis <webmaster@masize.com>
2024-03-18 00:11:35 -0300

    fix double call

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
