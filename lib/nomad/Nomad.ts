a868962c43cf5438696a2493beb2e5ddf272bec0
Agustin Kassis <webmaster@masize.com>
2024-03-18 00:13:05 -0300

    added nomad runtime logic

--- lib/nomad/Nomad.ts
+++ lib/nomad/Nomad.ts
@@ -1,34 +1,99 @@
-import { Relay } from "nostr-tools";
-import { NomadHandle, NomadRepoManifest } from "./types/nomad";
+import { EventEmitter } from "events";
+
+// Types
+import type { Event, Relay } from "nostr-tools";
+import type { NomadHandle, NomadRepoManifest } from "./types/nomad";
+import { NomadKinds } from "./types/kinds";
+import { NomadRuntime } from "./NomadRuntime";
 
 export class Nomad {
-  name: string;
-  constructor(eventId: string) {
-    this.name = "Nomad";
+  authorPubkey: string;
+  codeEvent: Event;
+  repoManifest?: NomadRepoManifest;
+  nomadRuntime: NomadRuntime | undefined;
+
+  // Not yet implemented
+  dependencies: { [name: string]: Nomad } = {};
+
+  public eventEmitter: EventEmitter = new EventEmitter();
+  public eventId: string;
+
+  constructor(codeEvent: Event, repoManifest?: NomadRepoManifest) {
+    this.codeEvent = codeEvent;
+    this.authorPubkey = codeEvent.pubkey;
+    this.repoManifest = repoManifest;
+    this.eventId = codeEvent.id;
   }
 
-  static async fromHandle(handle: string) {}
+  async init<T>(...args: any[]): Promise<T> {
+    this.nomadRuntime = new NomadRuntime(
+      this.codeEvent.content,
+      this.dependencies,
+      this.eventEmitter,
+      args
+    );
+
+    try {
+      await this.nomadRuntime.init();
+    } catch (e) {
+      console.error(e);
+      throw new Error("Error initializing Nomad Runtime");
+    }
+
+    this.eventEmitter.emit("nomad:init", args);
+
+    return this.nomadRuntime.getRuntimeInterface<T>();
+  }
+
+  async call<T>(name: string, ...args: any[]): Promise<T> {
+    this.eventEmitter.emit("nomad:call", [name, ...args]);
+
+    if (this.nomadRuntime === undefined) {
+      throw new Error(
+        "Nomad Runtime not yet initialized. Please call init() first"
+      );
+    }
+
+    return await this.nomadRuntime.call(name, ...args);
+  }
+
+  static async fromHandle(handleString: string, relay: Relay) {
+    const handle = parseHandle(handleString);
+    const authorPubkey = await getHandlePubkey(handle!);
+    const repoManifest = await getRepoManifest(
+      authorPubkey,
+      handle!.repo,
+      relay
+    );
+
+    // TODO: Proper sorting
+    const versions = Object.keys(repoManifest.versions).sort();
+    const latestVersion = versions[versions.length - 1];
+
+    const selectedVersion =
+      versions.find((v) => v === handle!.version) || latestVersion;
+    const eventId = repoManifest.versions[selectedVersion];
+
+    const codeEvent = await getCodeEvent(eventId, relay);
+
+    return new Nomad(codeEvent, repoManifest);
+  }
+
+  static async fromEventId(eventId: string, relay: Relay) {
+    const codeEvent = await getCodeEvent(eventId, relay);
+    return new Nomad(codeEvent);
+  }
+
+  static async fromHandleOrEventId(handleOrEventId: string, relay: Relay) {
+    if (handleOrEventId.includes("@")) {
+      return this.fromHandle(handleOrEventId, relay);
+    } else {
+      return this.fromEventId(handleOrEventId, relay);
+    }
+  }
 }
 
-async function resolveHandle(idOrName: string) {
-  const str = idOrName.toLocaleLowerCase().trim();
-  const handle = parseHandle(str);
-
-  // Not a handle, validate ID
-  if (!handle) {
-    return str;
-  }
-
-  // Validate ID
-  const regex = /^[a-f0-9]{64}$/;
-  if (!str.match(regex)) {
-    throw new Error("Invalid ID");
-  }
-
-  // Start resolving handle
-}
-
-function parseHandle(handle: string): NomadHandle | null {
+export function parseHandle(handle: string): NomadHandle | null {
   // Define the regular expression with capturing groups
   const regex =
     /^(?:([a-zA-Z0-9]+)@)?(.+)\/([a-zA-Z0-9]+)(?:@(latest|\d+(?:\.\d+)*))?$/;
@@ -49,7 +114,7 @@ function parseHandle(handle: string): NomadHandle | null {
   }
 }
 
-async function getHandlePubkey(handle: NomadHandle): Promise<string> {
+export async function getHandlePubkey(handle: NomadHandle): Promise<string> {
   const res = await fetch(
     `https://${handle.domain}/.well-known/nostr.json?name=${handle.username}`
   );
@@ -58,12 +123,55 @@ async function getHandlePubkey(handle: NomadHandle): Promise<string> {
   return data.names[handle.username!] as string;
 }
 
-async function getRepoManifest(
+export async function getRepoManifest(
   authorPubkey: string,
   repoName: string,
   relay: Relay
 ): Promise<NomadRepoManifest> {
+  const event = await relay.get({
+    authors: [authorPubkey],
+    "#d": [`repo:${repoName}`],
+    kinds: [NomadKinds.REPO_MANIFEST],
+  });
 
-  relay.
-  return data;
+  if (!event) {
+    throw new Error("Repo manifest not found");
+  }
+
+  const versions: { [key: string]: string } = {};
+  event.tags
+    .find((tag) => tag[0].startsWith("version:"))
+    ?.forEach((tag) => {
+      const version = tag[0].split(":")[1];
+      const eventId = tag[1];
+      versions[version] = eventId;
+    });
+
+  return {
+    name: repoName,
+    description: event.tags.find((tag) => tag[0] === "description")?.[1] || "",
+    versions: versions,
+    event: event,
+  };
+}
+
+export async function getCodeEvent(
+  eventId: string,
+  relay: Relay
+): Promise<Event> {
+  const event = await relay.get({
+    ids: [eventId],
+  });
+
+  if (!event) {
+    throw new Error("Code Event not found");
+  }
+
+  if (event.kind !== NomadKinds.NOMAD_CODE) {
+    throw new Error(
+      `Invalid event kind. Expecting ${NomadKinds.NOMAD_CODE} but got ${event.kind}`
+    );
+  }
+
+  return event;
 }
