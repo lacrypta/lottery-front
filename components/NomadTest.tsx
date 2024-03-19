b85f5f0c43839933ad7ba85d2da109f93c7eb3bd
Agustin Kassis <webmaster@masize.com>
2024-03-18 22:52:34 -0300

    added lottery example script

--- components/NomadTest.tsx
+++ components/NomadTest.tsx
@@ -1,18 +1,49 @@
 import { useEffect } from "react";
 import { useNomad } from "../hooks/useNomad";
 
+type ILotteryABI = {
+  lottery: (seed: string, winners: number, players: string[]) => string[];
+};
+
 export const NomadTest = () => {
-  const { chucho } = useNomad<{ pipo: string; chucho: Function }>(
-    "f2d3534f40feff7a08681227af38be5d6c3351f56697e586a8fc00c1577d59bd"
+  const { lottery } = useNomad<ILotteryABI>(
+    "1054c92c697c85e1947119fea1445668aa1a7ea9f13dd87c36613b694b52d8e9"
   );
 
   useEffect(() => {
-    if (!chucho) {
+    if (!lottery) {
       return;
     }
 
-    chucho();
-  }, [chucho]);
+    const seed = "some seed";
+    const winnersCount = 5;
+    const players = [
+      "Alice",
+      "Bob",
+      "Charlie",
+      "Dave",
+      "Eve",
+      "Frank",
+      "Grace",
+      "Heidi",
+      "Ivan",
+      "Judy",
+      "Mallory",
+      "Nina",
+      "Oscar",
+      "Peggy",
+      "Rupert",
+      "Sybil",
+      "Ted",
+      "Victor",
+      "Walter",
+    ];
+
+    const winners = lottery(seed, winnersCount, players);
+
+    console.info("winners:");
+    console.dir(winners);
+  }, [lottery]);
 
   return <div>hola</div>;
 };
