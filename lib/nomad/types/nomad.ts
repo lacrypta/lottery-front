09c1963742059ab97b544498413668c9eb6e7248
Agustin Kassis <webmaster@masize.com>
2024-03-19 23:05:54 -0300

    added nomad status

--- lib/nomad/types/nomad.ts
+++ lib/nomad/types/nomad.ts
@@ -13,3 +13,13 @@ export interface NomadRepoManifest {
   versions: { [key: string]: string };
   event: Event;
 }
+
+export enum NomadStatus {
+  HANDLE_RESOLVE = "handle_resolve",
+  REPO_MENIFEST = "repo_manifest",
+  CODE_EVENT = "code_event",
+  IDLE = "idle",
+  BOOTING = "booting",
+  RUNNING = "running",
+  ERROR = "error",
+}
