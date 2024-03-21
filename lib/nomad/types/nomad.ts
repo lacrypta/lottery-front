cbf940d54eef04685e265c20968e3717025f9348
Agustin Kassis <webmaster@masize.com>
2024-03-20 21:46:32 -0300

    removed type

--- lib/nomad/types/nomad.ts
+++ lib/nomad/types/nomad.ts
@@ -1,12 +1,5 @@
 import { Event } from "nostr-tools";
 
-export interface NomadHandle {
-  username?: string;
-  domain: string;
-  repo: string;
-  version?: string;
-}
-
 export interface NomadRepoManifest {
   name: string;
   description: string;
@@ -16,7 +9,7 @@ export interface NomadRepoManifest {
 
 export enum NomadStatus {
   HANDLE_RESOLVE = "handle_resolve",
-  REPO_MENIFEST = "repo_manifest",
+  REPO_MANIFEST = "repo_manifest",
   CODE_EVENT = "code_event",
   IDLE = "idle",
   BOOTING = "booting",
